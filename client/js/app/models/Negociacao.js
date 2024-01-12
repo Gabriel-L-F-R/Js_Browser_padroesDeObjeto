d7ce39cad2d124c6f169bb7d2d98c978f052ef66
Gabriel <gabrielfariareis@gmail.com>
2024-01-11 23:21:31 -0300

    Programacao defensiva

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,12 +1,14 @@
 class Negociacao {
-    constructor(data, quantidade, valor) {
-        this._date = data
+    constructor(date, quantidade, valor) {
+
+        this._date = new Date(date.getTime())
         this._quantidade = quantidade
         this._valor= valor
+        Object.freeze(this)
     }
 
     get volume() {return this.valor * this.quantidade}
-    get date() {return this._date}
+    get date() {return new Date(this._date.getTime())}
     get quantidade() {return this._quantidade}
     get valor() {return this._valor} 
 }
